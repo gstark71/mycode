a5ab7320fe04d94f4dc2d3ca40000ac9e8a162e6
Glenn Stark <gstark71@users.noreply.github.com>
2021-12-15 15:58:09 +0000

    writing code with lists and dicts

--- challenge01-munsters.py
+++ challenge01-munsters.py
@@ -12,31 +12,25 @@ munsters = {'endDate': 1966, 'startDate': 1964,\
 # ----------------------------------
 
 
-# print the value assoc with "endDate" key from the dict
-#print(munsters["endDate"])  # should display 1966
-print(munsters.get("endDate")) # does the same thing as line above, EXCEPT
-                               # upon not finding a key, returns NONE instead
-                               # of an ERROR
+# print the the end dat
+print(munsters.get("endDate")) 
+                        
+# print the start date
 
-# print the value assoc with "startDate" key from the dict
-#print("The Munsters began airing on:", munsters["startDate"])
 print("The Munsters began airing on:", munsters.get("startDate"))
 
-# print all names from the 3rd key
+# print all names from the 3rd key 
 print("Characters on the Munsters include:", munsters.get("names"))
 
-
 # Break down the data with a "loop"
-# define a new "list" named "names" for the purposes of teaching a simple loop
-names = ['Lily', 'Herman', 'Grandpa', 'Eddie', 'Marilyn']
+names = ['Eddie', 'Lily', 'Grandpa', 'Herman', 'Marilyn']
 
 # create a simple for loop
-for x in names:   # if a line ends in a colon the next line WILL be indented
+for x in names:   
     print(x, "is a character on the Munsters")
-    # print("---")  # print 3 dashes on the screen each time through the loop
+    # print("---")
 
 # we don't have to use a simple list to loop
-# we could access the nested list within the dictionary->   munsters.get('name')
 print("\nDisplays the same data, but by accessing the nested list within our dict\n")
 for x in munsters.get("names"):
     print(x, "is a character on the Munsters")

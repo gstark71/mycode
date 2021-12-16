beee449a7ddc16bf3246a900bbdcb5162e480d1a
Glenn Stark <gstark71@users.noreply.github.com>
2021-12-16 16:36:42 +0000

    Scripting Commands with Python

---
+++ lrnimport/intscript.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#  We want to only import the call and check_output functions from the subprocess module
+from subprocess import call
+call(["ip", "link", "show", "up"])
+print("This program will check your interfaces.")
+interface = input("Enter an interface, like, ens3: ")
+call(["ip", "addr", "show", "dev", interface])
+call(["ip", "route", "show", "dev", interface])
+
+
+
+
